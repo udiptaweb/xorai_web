f5a144080c00f880d768f540d9d523220fbb9638
Udipta Gogoi <gogoiudipta000@gmail.com>
2024-03-18 11:41:06 +0530

    navbar sticky

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
+      <div class="bg-black text-white sticky top-0 z-50">
       <nav className="w-full">
         <div className="mx-auto px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
@@ -48,6 +49,7 @@ export default function Home() {
           </div>
         </div>
       </nav>
+      </div>
       <main className="flex flex-col items-center justify-between">
         <div className="relative min-h-screen  flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
           <span className="flex items-baseline gap-x-1 "> <h1 className="text-5xl sm:text-9xl tracking-wide"> Xorai </h1> <span className="tracking-wider">TechLab</span> </span>
