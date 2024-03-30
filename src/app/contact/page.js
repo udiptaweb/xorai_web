ad9a228de7a3cf1c51e3b656beaa341a2dff7bed
Udipta Gogoi <gogoiudipta000@gmail.com>
2024-03-30 11:21:11 +0530

    &apos;

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -71,7 +71,7 @@ export default function Page() {
                 </div>
             </div>
             <div class="mt-10">
-                <button type="submit" class="block w-full rounded-md bg-cyan-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Let's talk</button>
+                <button type="submit" class="block w-full rounded-md bg-cyan-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Let&apos;s talk</button>
             </div>
         </form>
     </div>);
